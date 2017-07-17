c0e486d8736bcb0b3715c53f202424e2645068d2
Jeff Daily <jeff.daily@pnnl.gov>
2017-07-17 12:28:23 -0700

    fix argtypes in _make_nd_array for py3

--- ctypesgen.py
+++ ctypesgen.py
@@ -54,7 +54,7 @@ def _make_nd_array(c_pointer, shape, dtype=numpy.intc, order='C', own_data=True)
     if sys.version_info.major >= 3:
         buf_from_mem = ctypes.pythonapi.PyMemoryView_FromMemory
         buf_from_mem.restype = ctypes.py_object
-        buf_from_mem.argtypes = (ctypes.c_void_p, ctypes.c_int, ctypes.c_int)
+        buf_from_mem.argtypes = (ctypes.c_void_p, ctypes.c_ssize_t, ctypes.c_int)
         buffer = buf_from_mem(c_pointer, arr_size, 0x100)
     else:
         buf_from_mem = ctypes.pythonapi.PyBuffer_FromMemory
